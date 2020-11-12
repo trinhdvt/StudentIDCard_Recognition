a2895eaf3299e7df6a6f8f3b1a359d4f0ab82cba
trinhdvt <trinhvideo123@gmail.com>
2020-11-12 11:53:32 +0700

    resize img but keep aspect ratio

--- utils/utils.py
+++ utils/utils.py
@@ -18,28 +18,53 @@ def load_model():
     return cropper, detector, reader
 
 
-def resize_img(img):
+def resize_img(img, img_path=None):
+    if img_path:
+        img = cv2.imread(img_path)
     (h, w, _) = img.shape
-    if (w, h) > (cfg.IMG_WIDTH, cfg.IMG_HEIGHT):
-        if w > h:
-            img = cv2.resize(img, (cfg.IMG_WIDTH, cfg.IMG_HEIGHT))
-        else:
-            img = cv2.resize(img, (cfg.IMG_HEIGHT, cfg.IMG_WIDTH))
+    if h > cfg.IMG_HEIGHT:
+        ratio = cfg.IMG_HEIGHT / float(h)
+        dim = (int(ratio * w), cfg.IMG_HEIGHT)
+        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    # if w > cfg.IMG_WIDTH:
+    #     ratio = cfg.IMG_WIDTH / float(w)
+    #     dim = (cfg.IMG_WIDTH, int(ratio * h))
+    #     img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    # if (w, h) > (cfg.IMG_WIDTH, cfg.IMG_HEIGHT):
+    #     if w > h:
+    #         img = cv2.resize(img, (cfg.IMG_WIDTH, cfg.IMG_HEIGHT))
+    #     else:
+    #         img = cv2.resize(img, (cfg.IMG_HEIGHT, cfg.IMG_WIDTH))
     return img
 
 
-def cv2img_to_base64(img_path: str):
-    img = cv2.imread(img_path)
-    cv2_img = resize_img(img)
-    _, im_arr = cv2.imencode(".jpg", cv2_img)
+def cv2img_to_base64(img_path, img=None, resize=True):
+    if img is None:
+        img = cv2.imread(img_path)
+    if resize:
+        img = resize_img(img)
+    _, im_arr = cv2.imencode(".jpg", img)
     im_bytes = im_arr.tobytes()
 
     return base64.b64encode(im_bytes)
 
 
-def base64_to_cv2img(b64_encoded: str):
+def base64_to_cv2img(b64_encoded: str, save_path=None):
     assert b64_encoded is not None
     b64_decoded = base64.b64decode(b64_encoded)
     img = np.frombuffer(b64_decoded, dtype=np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    return img
+    if not save_path:
+        return img
+    else:
+        cv2.imwrite(save_path, img)
+# def resize_ratio(img_path, width):
+#     img = cv2.imread(img_path)
+#     (h, w, _) = img.shape
+#     ratio = width / float(w)
+#     dim = (width, int(ratio * h))
+#     resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+#     cv2.imwrite("../x.jpg", resized)
+#
+#
+# resize_ratio("../resized_img/hien1.jpg", 768)
