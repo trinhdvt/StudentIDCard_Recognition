10671a05c3fbf6771a4cc2b87500829b149c8b33
trinhdvt <trinhvideo123@gmail.com>
2020-11-25 17:21:03 +0700

    process img from request and send to web server

--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,12 @@ from detector.Detector import Detector
 from reader.Reader import Reader
 from reader.Config import Cfg
 from utils import config as cfg
+from datetime import datetime
+from utils import config
 import cv2
 import numpy as np
 import base64
+import os
 
 
 def load_model():
@@ -54,10 +57,44 @@ def base64_to_cv2img(b64_encoded: str, save_path=None):
     b64_decoded = base64.b64decode(b64_encoded)
     img = np.frombuffer(b64_decoded, dtype=np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
     if not save_path:
         return img
     else:
         cv2.imwrite(save_path, img)
+
+
+def save_to_local(bsx_data: tuple, sv_data: tuple) -> tuple:
+    """
+
+    :param bsx_data: (bsx_text, bsx_image)
+    :param sv_data:  (mssv_text, mssv_image)
+    :return: (bsx_img_path, mssv_img_path)
+    """
+    bsx_text, bsx_image = bsx_data
+    mssv_text, mssv_image = sv_data
+    bsx_path, mssv_path = generate_img_path(bsx_text, mssv_text)
+    base64_to_cv2img(bsx_image, bsx_path)
+    base64_to_cv2img(mssv_image, mssv_path)
+    return bsx_path, mssv_path
+
+
+def generate_img_path(*content):
+    """
+
+    :param content: (bsx_text, mssv_text)
+    :return: unique path base on current time
+    """
+    current_time = datetime.now().strftime("%d/%m/%Y_%H_%M_%S")
+    img_paths = [f"{config.LOCAL_IMG_STORAGE}{c}_{current_time}.jpg" for c in content]
+    return img_paths
+
+
+def to_web_storage(*img_path):
+    for file_path in img_path:
+        os.system(f"scp {file_path} nghiapham@{config.WEB_IP}:{config.WEB_IMG_STORAGE}")
+        os.remove(file_path)
+
 # def resize_ratio(img_path, width):
 #     img = cv2.imread(img_path)
 #     (h, w, _) = img.shape
