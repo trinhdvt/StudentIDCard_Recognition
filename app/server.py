ca4306f4285e0fa67e9eae3bf7ebe55189d2b086
trinhdvt <trinhvideo123@gmail.com>
2020-12-08 21:35:34 +0700

    server now will run detector on origin img if it failed with cropped img

--- app/server.py
+++ app/server.py
@@ -1,11 +1,9 @@
-from utils.utils import base64_to_cv2img
-from utils.utils import cv2img_to_base64
-from flask import Flask
-from flask import request
+from tools.utils import base64_to_cv2img, cv2img_to_base64
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from PIL import Image
-from utils.utils import load_model
-from utils import config
+from tools.loader import load_model
+from tools import config
 import time
 
 global cropper
@@ -24,32 +22,39 @@ def index():
 
 @app.route(config.MSSV_API_NAME, methods=['POST'])
 @cross_origin()
-def run_for_your_life():
+def run_for_my_life():
     start = time.time()
     # get data from request
     img_b64 = request.form.get('image')
     # convert base64 string to img
-    img = base64_to_cv2img(img_b64)
+    origin_img = base64_to_cv2img(img_b64)
     # run module id recognize
-    cropped = cropper.predict(img, resize=False)
+    cropped = cropper.predict(origin_img, resize=False)
+    #
+    return_data = {
+        'mssv': None,
+        'time': None,
+        'image': None
+    }
+    #
     try:
         id_only_img, _ = detector.predict(cropped)
     except TypeError:
-        return "Failed!"
-    try:
-        assert id_only_img is not None
-        id_only_img = Image.fromarray(id_only_img)
-        mssv = reader.predict(id_only_img)
-        end = time.time()
-        # send back data
-        return_data = {
-            'mssv': mssv,
-            'time': end - start,
-            'image': cv2img_to_base64(None, cropped, False).decode('utf-8')
-        }
-        return return_data
-    except AssertionError:
-        return "Failed!"
+        # don't stop, try it again
+        try:
+            id_only_img, _ = detector.predict(origin_img)
+        except TypeError:
+            return return_data
+    id_only_img = Image.fromarray(id_only_img)
+    mssv = reader.predict(id_only_img)
+    end = time.time()
+    # send back data
+    return_data = {
+        'mssv': mssv,
+        'time': end - start,
+        'image': cv2img_to_base64(None, cropped, False).decode('utf-8')
+    }
+    return return_data
 
 
 if __name__ == '__main__':
