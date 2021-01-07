d451f302bcfd5b44b4fe6b1ee59244f6ab50f81f
trinhdvt <trinhvideo123@gmail.com>
2021-01-07 14:58:35 +0700

    update config

--- tools/config.py
+++ tools/config.py
@@ -1,28 +1,19 @@
 # -------------------- SYSTEM CONFIG --------------------
-import platform
+import os
 
-ROOT_CONTENT = None
-if ROOT_CONTENT is None:
-    if platform.system() == 'Linux':
-        ROOT_CONTENT = "/home/dvt/Desktop/StudentIDCard_Recognition"
-    elif platform.system() == 'Windows':
-        ROOT_CONTENT = "C:/Users/congp/Desktop/StudentIDCard_Recognition"
-    else:
-        raise Exception("OS not support!")
+ROOT_CONTENT = os.getcwd()
 # -------------------- CROPPER CONFIG --------------------
 IMG_WIDTH = 768
-IMG_HEIGHT = 480
 # -------------------- DETECTOR CONFIG --------------------
 DETECTOR_CFG = f"{ROOT_CONTENT}/detector/config/detector_model.cfg"
 DETECTOR_WEIGHT = f"{ROOT_CONTENT}/detector/config/detector_model.weights"
-DETECTOR_WEIGHT_DRIVE_ID = "1KJ5gMSXaUPNoRKt9auEa2-EGC_zkA7XL"
+DETECTOR_WEIGHT_DRIVE_ID = "1xoO3xFSZ4KnCs4IRX3l4MyBGPVd4pmWb"
 DETECTOR_LABELS = f"{ROOT_CONTENT}/detector/config/labels"
+DISPLAY_FONT = f"{ROOT_CONTENT}/detector/config/Montserrat-SemiBold.ttf"
 # -------------------- READER CONFIG --------------------
 READER_CFG = f"{ROOT_CONTENT}/reader/config/config.yml"
-READER_WEIGHT = f"{ROOT_CONTENT}/reader/config/transformerocr3.pth"
-READER_BACKUP_WEIGHT = f"{ROOT_CONTENT}/reader/config/transformerocr2.pth"
-READER_WEIGHT_DRIVE_ID = "1_nhGbmnqxQMy0XYD2uVjWqKBQwNhAIZn"
-READER_BACKUP_WEIGHT_DRIVE_ID = "1yTpMg07Xizg_rY9oHNyBmz7At-Uak2tS"
+READER_WEIGHT = f"{ROOT_CONTENT}/reader/config/transformerocr.pth"
+READER_WEIGHT_DRIVE_ID = "1k08K0f-r6VOGvlZ1EyF4DL6MdnOJX_PK"
 DEVICE = "cpu"
 # -------------------- WEB_SERVER CONFIG --------------------
 MSSV_API_NAME = "/id_recognize"
