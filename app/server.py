8d0b7e980ff92fb90eec95e4733e2086750ae6a3
trinhdvt <trinhvideo123@gmail.com>
2021-02-07 20:59:29 +0700

    modified API

--- app/server.py
+++ app/server.py
@@ -1,36 +1,31 @@
+import os
 import time
-import numpy as np
+
 import cv2
-from flask import Flask, request
+import numpy as np
+from PIL import Image
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
+
 from tools import config
 from tools.loader import load_model
-from tools.utils import base64_to_cv2img, cv2img_to_base64, cv2img_to_pil
-from PIL import ImageDraw, ImageFont
+from tools.utils import cv2img_to_base64, cv2img_to_pil
 
 global cropper
 global detector
 global reader
 
 app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+app.static_folder = 'static'
 CORS(app)
 
 
-@app.route("/", methods=['GET', 'POST'])
-@cross_origin()
-def index():
-    return "Hello World", 200
-
-
-@app.route(config.MSSV_API_NAME, methods=['POST'])
-@cross_origin()
-def run_for_my_life():
+def run_for_my_life2(cv2_img):
     #
     start = time.time()
-    # get data from request
-    img_b64 = request.form.get('image')
-    # convert base64 string to img
-    origin_img = base64_to_cv2img(img_b64)
+    origin_img = cv2_img
     #
     try:
         cropped_img = cropper.predict(origin_img, resize=True)
@@ -41,28 +36,45 @@ def run_for_my_life():
         for key, value in detect_rs.items():
             text = reader.predict(cv2img_to_pil(value))
             reader_rs[key] = text
-            #
-            (x, y, w, h) = coordinate[key]
-            draw_img = ImageDraw.Draw(cropped_img)
-            draw_img.text((x, y - 5), text, fill=(255, 0, 0),
-                          font=ImageFont.truetype(config.DISPLAY_FONT, 18))
     except Exception as e:
-        print(e)
-        return "", 406
+        return {'massage': str(e)}, 406
     #
     end = time.time()
     # send back data
     cropped_img = cv2.cvtColor(np.array(cropped_img), cv2.COLOR_RGB2BGR)
     return_data = {
-        'result': reader_rs,
         'time': end - start,
         'image': cv2img_to_base64(None, cropped_img, False).decode('utf-8')
-        }
+    }
+    return_data.update(reader_rs)
     return return_data, 200
+
+
+@app.route("/", methods=['GET', 'POST'])
+@cross_origin()
+def index():
+    return render_template('index.html')
+
+
+@app.route("/uploadImage", methods=['POST'])
+@cross_origin()
+def upload_image():
+    if 'image-file' in request.files:
+        image_data = request.files['image-file']
+        pil_img = Image.open(image_data)
+        pil_img.save("image.jpg", format="JPEG")
+        cv2_img = cv2.imread("image.jpg")
+        #
+        response, status_code = run_for_my_life2(cv2_img)
+        #
+        os.remove("image.jpg")
+        #
+        return jsonify(response), status_code
+    return jsonify({'message': 'failed'}), 200
 
 
 if __name__ == '__main__':
     print("Model is loading")
     cropper, detector, reader = load_model()
     print("Model is ready!")
-    app.run(debug=True, host=config.MSSV_API_ADDRESS, port=config.API_PORT)
+    app.run(debug=True, host='localhost', port=config.API_PORT)
